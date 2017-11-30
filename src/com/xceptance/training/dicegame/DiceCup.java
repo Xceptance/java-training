59c617b291a55438c7b6560af06f8847f4ca08ca
Rene Schwietzke <r.schwietzke@xceptance.net>
2017-11-30 10:03:16 +0100

    First dice cup

--- src/com/xceptance/training/dicegame/DiceCup.java
+++ src/com/xceptance/training/dicegame/DiceCup.java
@@ -1,13 +1,24 @@
 package com.xceptance.training.dicegame;
 
+import java.util.List;
+
 public class DiceCup
 {
     // list of dice to use
+    
     
     /**
      * Constructor, takes a list of dice to be used
      */
     public DiceCup(List<Dice> dice)
+    {
+        
+    }
+    
+    /**
+     * Rolls all dice
+     */
+    public void roll()
     {
         
     }
